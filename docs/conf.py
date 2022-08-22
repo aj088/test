5912eb484369443f4e38d8256995f11b0fc9687d
aj088 <55105250+aj088@users.noreply.github.com>
2022-08-22 17:00:34 -0400

    delete extension

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,7 @@ release = 'test'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark',
-    'sphinx_markdown_tables'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
